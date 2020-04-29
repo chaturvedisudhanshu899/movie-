b4eccafc1f6097b641cd88e137a3b133ffeacaf9
chaturvedisudhanshu899 <chaturvedisudhanshu899@gmail.com>
2020-04-29 15:07:45 +0530

    first commit

---
+++ main/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Movie(models.Model):
+    name=models.CharField(max_length=300)
+    director=models.CharField(max_length=300)
+    cast=models.CharField(max_length=300)
+    description=models.TextField(max_length=5000)
+    release_date=models.DateField()
+    averageRating=models.FloatField()
+    image=models.URLField(default=None,null=True)
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
---
+++ main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+# Create your views here.
+def  home(request):
+    allMovies=Movie.objects.all()
+
+    contest ={
+        "movies":allMovies,
+    }
+    return render(request,'main/index.html',contest)
+
+def detail(request,id):
+    movie=Movie.objects.get(id=id)
+
+    context={
+        "movie":movie
+    }
+    return render(request,'main/details.html',context)
